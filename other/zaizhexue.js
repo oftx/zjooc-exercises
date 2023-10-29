0f116cd347670c25721227e5b82461ea7ae1c69d
Anom <3204417902@qq.com>
2023-10-29 18:13:08 +0800

    updated

--- other/zaizhexue.js
+++ other/zaizhexue.js
@@ -1,60 +1,69 @@
 // ==UserScript==
-// @name         zai zhe xue
+// @name         获取在浙学页面上的题目和答案并输出到控制台
 // @version      1.0
-// @description  Get the text inside the paragraph element contained in the element with class "processing_img"
+// @description  Get the questions and answers on the page of zjooc and output to the console
 // @match        https://www.zjooc.cn/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+    setTimeout(function () {
+        var questionElements = document.querySelectorAll('div.questiono-item.el-col.el-col-24');
+        var titleIndexes = [];
+        var questionTitleElements = [];
+        var selectionElements1 = [];
+        var selectionElements2 = [];
+        var myAnswers = [];
+        var checkedAnswers = [];
+        var scores = [];
+        var myScores = [];
+        var text = '';
 
-    let i = 0;
-    let count = 0;
-    let count2 = 1;
-    const char = ["A", "B", "C", "D"];
-    let tempStr = "";
-    let str = "";
-
-    setTimeout(function() {
-        const processingImgs = document.querySelectorAll('.processing_img');
-        // const targetElement = document.querySelectorAll('.el-col.el-col-24 > .common_test_correct'); // 有答案时
-        const targetElement = document.querySelectorAll('.el-col.el-col-24 > .common_test_myanswer'); // 无答案时
-        const scoreElement = document.querySelectorAll('.el-col.el-col-24 > .common_test_score'); // 无答案时
-
-        processingImgs.forEach(function(processingImg) {
-            // const paragraph = processingImg.querySelector('p');
-            const paragraph = processingImg;
-            const text = paragraph.textContent.trim();
-            i++;
-            if (i % 5 == 1) { // 题目
-                str += "- - -\n";
-                tempStr = ((count2 - 1) % 10) + 1 + ". " + text + "  ";
-                console.log(tempStr);
-                str += ((count2 - 1) % 10) + 1 + ". __" + text + "__";
-                count2++;
-            } else { // 选项
-                tempStr = char[(i - 1) % 5 - 1] + ". " + text;
-                console.log(tempStr);
-                str += tempStr;
-                if (targetElement[count - 1].nextElementSibling.textContent.trim().includes(char[(i - 1) % 5 - 1])) {
-                    str += " ```✓```";
+        questionElements.forEach(function (questionElement) {
+            titleIndexes.push(questionElement.querySelector('b.fl'));
+            questionTitleElements.push(questionElement.querySelector('span.processing_img'));
+            let radios = questionElement.querySelectorAll('span.el-radio__label');
+            let checks = questionElement.querySelectorAll('span.el-checkbox__label');
+            let checked = [];
+            selectionElements1.push(radios);
+            selectionElements2.push(checks);
+            if (radios.length > 0) {
+                for (var i = 0; i < radios.length; i++) {
+                    checked.push(questionElement.querySelectorAll('label.el-radio.mr5')[i].classList.contains("is-checked"));
                 }
-                str += "  \n";
-                if (i % 5 == 0) str += "\n";
-            }
-
-            if (i % 5 == 1) { // 答案
-                const targetContent = targetElement[count].textContent.trim() + ' ' + targetElement[count].nextElementSibling.textContent.trim();
-                console.log(targetContent);
-                str += "*```" + targetElement[count].nextElementSibling.textContent.trim() + "```*";
-                if (scoreElement[count].nextElementSibling.textContent.trim() == "0") {
-                    str += " **```此题答案错误```**";
+            } else if (checks.length > 0) {
+                for (var j = 0; j < checks.length; j++) {
+                    checked.push(questionElement.querySelectorAll('label.el-checkbox.mr5')[j].classList.contains("is-checked"));
                 }
-                str += "  \n";
-                count++;
             }
+            myAnswers.push(questionElement.querySelectorAll('span.common_test_text'));
+            scores.push(questionElement.querySelector('div.fr'));
+            myScores.push(document.querySelectorAll('div.questiono-item.el-col.el-col-24')[0].querySelector('div.common_test_answer').querySelectorAll('span.common_test_text')[1]);
+            checkedAnswers.push(checked);
         });
-        console.log(str);
-    }, 3000);
+
+        for (var i = 0; i < questionTitleElements.length; i++) {
+            let score = scores[i].innerText.replace('分', '');
+            let myScore = myScores[i].innerText;
+            let isCorrect = score == myScore;
+            let sign = isCorrect ? '```√```' : '```×```';
+            let title = titleIndexes[i].innerText + ' __' + questionTitleElements[i].innerText + "__*```" + myAnswers[i][0].innerText + "```*  "; // + " " + myScores[i].innerText + "/" + scores[i].innerText.replace('分', '')
+            console.log(title); // question title
+            text += '- - -\n' + title + '\n';
+            for (var j = 0; j < selectionElements1[i].length; j++) { // selection
+                let selection = selectionElements1[i][j].innerText.trim() + " " + `${checkedAnswers[i][j] ? sign : ''}` + "  ";
+                console.log(selection);
+                text += selection + '\n';
+            }
+            for (var k = 0; k < selectionElements2[i].length; k++) { // selection
+                if (!isCorrect) sign = '```?```';
+                let selection = selectionElements2[i][k].innerText.trim() + " " + `${checkedAnswers[i][k] ? sign : ''}` + "  ";
+                console.log(selection);
+                text += selection + '\n';
+            }
+            text += '\n';
+        }
+
+        console.log(text);
+    }, 5000);
 })();
